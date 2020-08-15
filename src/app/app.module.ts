ae562d98450742387d6eff40046c23e68f78f217
Shy Agam <us@bespunky.io>
2020-08-15 14:56:54 +0300

    ✨ Added a simple map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {GoogleMapsModule } from '@bespunky/angular-google-maps/async';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        GoogleMapsModule.forRoot({ 'apiUrl': {key: ''}})
   ],
   providers: [],
   bootstrap: [AppComponent]
